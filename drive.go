4a7f491c21446fc0dd960ee96d1cf67d457ca09e
Jim Whitehead <jnwhiteh@gmail.com>
2014-06-05 23:20:33 +0200

    Add basic remote backup capability

--- drive.go
+++ drive.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 
 	"code.google.com/p/google-api-go-client/drive/v2"
 )
 
-func getFolderByName(service *drive.Service, name string) *drive.File {
+func findFolder(service *drive.Service, name string) *drive.File {
 	query := fmt.Sprintf("mimeType = 'application/vnd.google-apps.folder' and title = '%s' and trashed != true", name)
 	fileList, err := service.Files.List().Q(query).Do()
 	if err != nil {
@@ -22,4 +24,69 @@ func getFolderByName(service *drive.Service, name string) *drive.File {
 	}
 
 	return fileList.Items[0]
+}
+
+func newFileWithParent(name string, parentId string) *drive.File {
+	driveFile := drive.File{
+		Title: name,
+	}
+	if parentId != "" {
+		driveFile.Parents = []*drive.ParentReference{
+			&drive.ParentReference{Id: parentId},
+		}
+	}
+	return &driveFile
+}
+
+func mkdir(service *drive.Service, name string, parentId string) (*drive.File, error) {
+	driveFile := newFileWithParent(name, parentId)
+	driveFile.MimeType = "application/vnd.google-apps.folder"
+	return service.Files.Insert(driveFile).Do()
+}
+
+func uploadFile(service *drive.Service, name string, parentId, localFilename string) (*drive.File, error) {
+	localFile, err := os.Open(localFilename)
+	if err != nil {
+		return nil, err
+	}
+	defer localFile.Close()
+
+	driveFile := newFileWithParent(name, parentId)
+	return service.Files.Insert(driveFile).Media(localFile).Do()
+}
+
+func getRemoteFiles(service *drive.Service, parentId string) ([]*drive.File, error) {
+	if parentId == "" {
+		parentId = "root"
+	}
+
+	var files []*drive.File
+	childCall := service.Children.List(parentId).Q("mimeType != 'application/vnd.google-apps.folder' and trashed != true")
+	childList, err := childCall.Do()
+	for childList != nil && childList.Items != nil {
+		if err != nil {
+			return nil, err
+		}
+		for _, child := range childList.Items {
+			file, err := service.Files.Get(child.Id).Do()
+			if err != nil {
+				return nil, err
+			}
+			files = append(files, file)
+		}
+		if childList.NextPageToken == "" {
+			break
+		}
+		childList, err = childCall.PageToken(childList.NextPageToken).Do()
+	}
+
+	return files, nil
+}
+
+func toJson(val interface{}) string {
+	buf, err := json.Marshal(val)
+	if err != nil {
+		return ""
+	}
+	return string(buf)
 }
--- main.go
+++ main.go
@@ -16,6 +16,7 @@ var (
 	localDir         = flag.String("local_dir", ".", "The directory on your local machine that should be backed up")
 	remoteFolderName = flag.String("remote_folder", "gdrive-backups", "The name of the folder on Google Drive to use for backups")
 	createRemote     = flag.Bool("create_remote", true, "Create the remote directory if it does not already exist")
+	workers          = flag.Int("workers", 1, "Number of concurrent uploads")
 )
 
 func main() {
@@ -28,26 +29,74 @@ func main() {
 		log.Fatalf("Failed to create Drive client: %v", err)
 	}
 
-	remoteFolder := getFolderByName(service, *remoteFolderName)
+	remoteFolder := findFolder(service, *remoteFolderName)
 	if remoteFolder == nil {
 		if *createRemote {
 			log.Printf("Creating new remote folder %s", *remoteFolderName)
-			driveFile := drive.File{
-				Title:    *remoteFolderName,
-				MimeType: "application/vnd.google-apps.folder",
-			}
-
-			remoteFolder, err = service.Files.Insert(&driveFile).Do()
-			if err != nil {
-				log.Fatalf("Failed to create remote folder %s", *remoteFolderName)
-			}
+			remoteFolder, err = mkdir(service, *remoteFolderName, "")
 		}
 	} else {
 		fmt.Printf("Found folder: %v", remoteFolder)
 	}
 
 	localFiles := getLocalFiles(*localDir)
+	fmt.Printf("Local files:\n")
 	for _, file := range localFiles {
 		fmt.Printf("%s\t%s\n", file.md5, file.filename)
+	}
+
+	remoteFiles, err := getRemoteFiles(service, remoteFolder.Id)
+	if err != nil {
+		log.Fatalf("Could not fetch remote files: %s", err)
+	}
+
+	fmt.Printf("Remote files:\n")
+	for _, file := range remoteFiles {
+		fmt.Printf("%s\t%s\n", file.Md5Checksum, file.Title)
+	}
+
+	fileHashes := make(map[string]bool)
+	for _, file := range remoteFiles {
+		fileHashes[file.Md5Checksum] = true
+	}
+
+	var toUpload []localFile
+	for _, file := range localFiles {
+		if present, ok := fileHashes[file.md5]; !ok || !present {
+			toUpload = append(toUpload, file)
+		}
+	}
+
+	log.Printf("Skipping %d files, uploading %d", len(localFiles)-len(toUpload), len(toUpload))
+	if len(toUpload) > 0 {
+		work := make(chan localFile, 1)
+		done := make(chan struct{}, len(toUpload))
+		for i := 0; i < *workers; i++ {
+			go uploadWorker(service, remoteFolder, work, done)
+		}
+
+		for _, file := range toUpload {
+			select {
+			case work <- file:
+			}
+		}
+
+		for i := 0; i < len(toUpload); i++ {
+			<-done
+		}
+	} else {
+		log.Printf("No files to upload")
+	}
+}
+
+func uploadWorker(service *drive.Service, remoteFolder *drive.File, work <-chan localFile, done chan struct{}) {
+	for file := range work {
+		log.Printf("Uploading %s", file.filename)
+		_, err := uploadFile(service, file.Name(), remoteFolder.Id, file.filename)
+		if err != nil {
+			log.Fatalf("Failed when uploading file %s: %s", file.filename, err)
+		}
+		log.Printf("Finished uploading %s", file.filename)
+		done <- struct{}{}
 	}
 }
